be5fc8d9d6b4d7fb57d3d89c69e5a4bc16201f90
Sagar Avhad <avhad.avhad@gmail.com>
2019-10-12 12:43:34 +0530

    added switches in app for selecting light, dark and system modes.

--- app/src/main/java/com/raywenderlich/airlock/AirlockApplication.kt
+++ app/src/main/java/com/raywenderlich/airlock/AirlockApplication.kt
@@ -31,11 +31,20 @@
 package com.raywenderlich.airlock
 
 import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 class AirlockApplication : Application() {
-
   override fun onCreate() {
     super.onCreate()
+      val pref = getSharedPreferences(Constants.PREFS_MODE, Context.MODE_PRIVATE)
+      val nightMode = when (pref.getInt(Constants.MODE_KEY, 0)) {
+          Mode.LIGHT.ordinal -> AppCompatDelegate.MODE_NIGHT_NO
+          Mode.DARK.ordinal -> AppCompatDelegate.MODE_NIGHT_YES
+          Mode.SYSTEM.ordinal -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+          else -> AppCompatDelegate.MODE_NIGHT_NO
+      }
+      AppCompatDelegate.setDefaultNightMode(nightMode)
 
   }
 }
--- app/src/main/java/com/raywenderlich/airlock/SettingsFragment.kt
+++ app/src/main/java/com/raywenderlich/airlock/SettingsFragment.kt
@@ -30,45 +30,63 @@
 
 package com.raywenderlich.airlock
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : DialogFragment(), RadioGroup.OnCheckedChangeListener {
-  
-  override fun onStart() {
-    super.onStart()
 
-    dialog?.window?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
-  }
+    private lateinit var pref: SharedPreferences
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_settings, container, false)
-  }
+    override fun onStart() {
+        super.onStart()
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    modeGroup.setOnCheckedChangeListener(this)
-
-  }
-
-  override fun onCheckedChanged(rg: RadioGroup?, checkedId: Int) {
-    when (checkedId) {
-      R.id.light -> Log.i("SettingsFragment", "Light")
-      R.id.dark -> Log.i("SettingsFragment", "Dark")
-      R.id.system -> Log.i("SettingsFragment", "System")
+        dialog?.window?.setLayout(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
     }
-  }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pref = activity!!.getSharedPreferences(Constants.PREFS_MODE, Context.MODE_PRIVATE)
+        when (pref.getInt(Constants.MODE_KEY, 0)) {
+            Mode.LIGHT.ordinal -> light.isChecked = true
+            Mode.DARK.ordinal -> dark.isChecked = true
+            Mode.SYSTEM.ordinal -> system.isChecked = true
+            else -> light.isChecked = true
+        }
+        modeGroup.setOnCheckedChangeListener(this)
+
+    }
+
+    override fun onCheckedChanged(rg: RadioGroup?, checkedId: Int) {
+        when (checkedId) {
+            R.id.light -> switchToMode(AppCompatDelegate.MODE_NIGHT_NO, Mode.LIGHT)
+            R.id.dark -> switchToMode(AppCompatDelegate.MODE_NIGHT_YES, Mode.DARK)
+            R.id.system -> switchToMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, Mode.SYSTEM)
+        }
+    }
+
+    private fun switchToMode(nightMode: Int, mode: Mode) {
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+        pref.edit().putInt(Constants.MODE_KEY, mode.ordinal).apply()
+    }
 
 }
